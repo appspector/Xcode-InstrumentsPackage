641aba1a327833e84bb618f5c215e20b8f19fec5
Deszip <raf.rafiki@gmail.com>
2019-01-27 09:32:05 +0200

    Basic tracing implementation

--- Package/ViewController.swift
+++ Package/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import os.signpost
+
+struct Scope {
+    let name: String
+    let ID: String
+}
+
+struct Span {
+    let name: String
+    let ID: String
+    let scopeID: String
+}
 
 class ViewController: UIViewController {
 
+    static let log = OSLog(subsystem: "com.tracer", category: "Behavior")
+    static let signpostID = OSSignpostID(log: ViewController.log, object: self as AnyObject)
+    
+    var scopeCounter: Int = 0
+    var spanCounter: Int = 0
+    var currentScope: Scope?
+    var currentSpan: Span?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: Actions
+    
+    @IBAction func openNewScope(_ sender: Any) {
+        if currentScope == nil {
+            scopeCounter += 1
+            currentScope = openScope("\(scopeCounter)")
+        }
+    }
+    
+    @IBAction func startNewSpan(_ sender: Any) {
+        if currentSpan == nil {
+            guard let scope = currentScope else { return }
+            spanCounter += 1
+            currentSpan = startSpan("Span: \(spanCounter)", inScope: scope)
+        }
+    }
+    
+    @IBAction func stopCurrentSpan(_ sender: Any) {
+        guard let span = currentSpan else { return }
+        stopSpan(span)
+        currentSpan = nil
+    }
+    
+    @IBAction func closeCurrentScope(_ sender: Any) {
+        guard let scope = currentScope else { return }
+        closeScope(scope)
+        currentScope = nil;
+    }
+    
+    // MARK: API
+    
+    func openScope(_ name: String) -> Scope {
+        let scope = Scope(name: name, ID: UUID().uuidString)
+        os_signpost(.begin, log: ViewController.log, name: "tracing", signpostID: ViewController.signpostID, "scope-open: %{public}@", scope.name)
+        
+        print("Scope opened")
+        
+        return scope
+    }
+    
+    func closeScope(_ scope: Scope) {
+        os_signpost(.end, log: ViewController.log, name: "tracing", signpostID: ViewController.signpostID, "scope-close: %{public}@", scope.name)
+        
+        print("Scope closed")
+    }
+    
+    func startSpan(_ name: String, inScope scope: Scope) -> Span {
+        let span = Span(name: name, ID: UUID().uuidString, scopeID: scope.ID)
+        
+        os_signpost(.begin, log: ViewController.log, name: "tracing", signpostID: ViewController.signpostID, "span-start: %{public}@,scope-id:%{public}@", span.name, span.scopeID)
+        
+        return span
+    }
+    
+    func stopSpan(_ span: Span) {
+        os_signpost(.end, log: ViewController.log, name: "tracing", signpostID: ViewController.signpostID, "span-stop: %{public}@,scope-id:%{public}@", span.name, span.scopeID)
+    }
 
 }
 
